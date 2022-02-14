de07dd10f6d3b4391ecabacb748caba8204df8d7
Manuela Grajales Duque <mgrajale@everis.nttdata.com>
2022-02-14 14:08:26 +0100

    setting up the requests methos to persist .rdf documents in Virtuoso as graphs

--- dev/cfg/crud.py
+++ dev/cfg/crud.py
@@ -4,7 +4,8 @@ VIRTUOSO_EIRA_LOAD_RDF_FILE = {
     "source": "." + ctt.EIRA_THESAURUS_DETAILS.get('path'),
     "user": "dba",
     "password": "dba",
-    "graph": "eira",
-    "endpoint": "http://localhost:8890/sparql-graph-crud-auth?",
+    "graph": "http://eira.org/",
+    "endpoint": "http://localhost:8890/sparql-graph-crud-auth?graph-uri=http://data.europa.eu/dr8/mirror3/",
+    # "endpoint": "http://localhost:8890/DAV/home/eira/source.rdf",
     "method": "graph-uri"
     }
--- dev/com/nttdata/dgi/persistence/virtuoso_persistor.py
+++ dev/com/nttdata/dgi/persistence/virtuoso_persistor.py
@@ -1,6 +1,6 @@
 from com.nttdata.dgi.persistence.persistor import Persistor
 import requests
-from requests.auth import HTTPBasicAuth
+from requests.auth import HTTPBasicAuth, HTTPDigestAuth
 
 
 class VirtuosoPersistor(Persistor):
@@ -8,20 +8,25 @@ class VirtuosoPersistor(Persistor):
     def __init__(self, persistor_configuration: dict):
         super(VirtuosoPersistor, self).__init__(persistor_configuration)
         self.url = self.persistor_configuration.get("endpoint")
-        self.url += self.persistor_configuration.get("method") + "="
-        self.url += self.persistor_configuration.get("graph")
+        # self.url += self.persistor_configuration.get("method") + "="
+        # self.url += self.persistor_configuration.get("graph")
         self.user = self.persistor_configuration.get("user")
         self.password = self.persistor_configuration.get("password")
 
     def __load(self):
 
-        file = self.persistor_configuration.get("source")
-        with open(file, 'r') as myfile:
+        # file = self.persistor_configuration.get("source")
+        file = "C:/SEMBUdev/Github/CAMSS/CAMSS-SIS/dev/arti/rdf/eira_thesaurus.rdf"
+        with open(file, 'r', encoding='utf8') as myfile:
             content_file = myfile.read()
-        result = requests.put(url=self.url, data=content_file, auth=HTTPBasicAuth(self.user, self.password))
-        if not result.ok:
-            self.report = {"message": f"FAILED: The file {file} could not be loaded onto the Virtuoso repository."
-                                      f" Response: {result.content}"}
+        try:
+            result = requests.post(url=self.url, data=content_file.encode('utf-8'),
+                                  auth=HTTPDigestAuth(self.user, self.password))
+            if not result.ok:
+                self.report = {"message": f"FAILED: The file {file} could not be loaded onto the Virtuoso repository."
+                                          f" Response: {result.content}"}
+        except Exception as x:
+            print(x)
 
         return super()
 
