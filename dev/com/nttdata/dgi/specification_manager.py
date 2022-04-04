8c9ff968c12cd1eda79633355c379de6c2cf73de
Manuela Grajales Duque <mgrajale@everis.nttdata.com>
2022-04-04 11:56:32 +0200

    changed the number of documents to download

--- dev/com/nttdata/dgi/specification_manager.py
+++
@@ -1,4 +0,0 @@
-class SpecificationManager:
-
-    def __init__(self):
-        return
--- dev/com/nttdata/dgi/vocabulary_manager.py
+++
@@ -1,9 +0,0 @@
-class Vocabulary:
-    list_vocabularies: list
-
-    def __init__(self):
-        return
-
-    def get_vocabularies(self, vocabularies_details) -> dict:
-
-        return []
